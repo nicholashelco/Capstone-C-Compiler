63c4055a64d1c489dd544ec6205c95f36143b2eb
nicholashelco <54865898+nicholashelco@users.noreply.github.com>
2024-01-31 13:48:48 -0500

    Add files via upload

---
+++ Test Programs/2) Syntax Errors/No Parens with if.c
@@ -0,0 +1,11 @@
+int main() {
+
+    int xt = 50;
+
+    if xt > 60 {
+
+        xt--;
+    }
+
+    return 0;
+}
---
+++ Test Programs/3) Semantic Errors/Float as Array Index.c
@@ -0,0 +1,9 @@
+int main() {
+
+    int arr[5] = {43, 12, 675, 23, 0};
+    
+    float f = 1.05f;
+    int x = arr[f];
+
+    return 0;
+}
